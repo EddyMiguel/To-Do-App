8e457f51b1b8098738aead268814026935ff237c
eddyvakdez <eddymiguelmartinez402@gmail.com>
2025-12-19 19:37:22 -0400

    mejoras en la estructura y estilo

--- app.js
+++ app.js
@@ -5,16 +5,33 @@ function loadTasks(){
   const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   list.innerHTML = "";
   tasks.forEach((t,i)=>{
-    list.innerHTML += `<li>${t} 
-      <button onclick="removeTask(${i})">X</button></li>`;
+    list.innerHTML += `
+      <li class="${t.done ? 'completed' : ''}">
+        <span>
+          <span class="badge">${i+1}</span>
+          ${t.text}
+        </span>
+        <div>
+          <button onclick="toggle(${i})">✔</button>
+          <button class="delete" onclick="removeTask(${i})">X</button>
+        </div>
+      </li>`;
   });
 }
 
 function addTask(){
+  if(input.value.trim()==="") return alert("Escribe algo");
   const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  tasks.push(input.value);
+  tasks.push({text:input.value, done:false});
   localStorage.setItem("tasks", JSON.stringify(tasks));
   input.value="";
+  loadTasks();
+}
+
+function toggle(i){
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks[i].done = !tasks[i].done;
+  localStorage.setItem("tasks", JSON.stringify(tasks));
   loadTasks();
 }
 
